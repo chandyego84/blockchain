726e50ed1792ca7d82ab1051ed66037d1c703f4e
chandyego <chandlerjuego101@gmail.com>
2022-12-08 12:45:58 -0600

    mysql connection. raw stuff.

--- app.py
+++ app.py
@@ -1,49 +1,72 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flaskext.mysql import MySQL
 from blockchain import Block, Blockchain
 
 app = Flask(__name__)
 appChain = Blockchain()
 
+# MySQL configs
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '#Rocketman84'
+app.config['MYSQL_DATABASE_DB'] = 'blockchain'
+mysql.init_app(app)
+
 # Home
 @app.route('/')
-def HomePage():
+def Home():
     return render_template('home.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def HomeActionResult():
     if request.method == 'POST':
+        # user chose a command
         requestAction = request.form
-
         if 'New Transaction' in requestAction:
-            return redirect(url_for('NewTransactionPage'))
+            return redirect(url_for('Transaction'))
         
         elif 'Mine' in requestAction:
             return redirect(url_for('MinePage'))
 
-        return render_template("home.html")
+    return render_template("home.html")
 
 # Transactions
-@app.route('/transactions/new')
-def NewTransactionPage():
-    print("A new transaction is being made")
-
-    return render_template('newTransaction.html')
-
 @app.route('/transactions', methods=['POST', 'GET'])
-def NewTransaction():
+def Transaction():
     if request.method == 'POST':
         requestAction = request.form
 
         # get transaction info from the request
         sender = requestAction['Sender']
         recipient = requestAction['Recipient']
-        amount = requestAction['Amount']
+        amount = float(requestAction['Amount'])
 
         # add the transaction to the blockchain
         appChain.NewTransaction(sender, recipient, amount)
-        transactions = appChain.currentTransactions
 
-        return render_template('transactions.html', requestAction=requestAction, transactions=transactions)
+        # insert into mySQL pending_transactions table
+        # Connect to the database
+        conn = mysql.connect()
+        # Create a cursor
+        curs = conn.cursor()
+        curs.execute('''
+            INSERT INTO pending_transactions (sender, recipient, amount) 
+            VALUES (%s, %s, %s)''', (sender, recipient, amount))
+        
+        # save to db
+        conn.commit()
+        # Close the cursor and connection
+        curs.close()
+        conn.close()
+
+        return redirect('/transactions')
+    
+    else:
+        # show all pending transactions
+        transactions = appChain.currentTransactions
+        return render_template('transactions.html', transactions=transactions)
+    
 
 # Mining
 @app.route('/mine')
