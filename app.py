bef2c710bb29f879c38196541e58f0011bff8608
chandyego <chandlerjuego101@gmail.com>
2023-01-17 11:32:42 -0800

    adding more comments to app file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from blockchain import Block, Blockchain
 app = Flask(__name__)
 appChain = Blockchain()
 
-# MySQL configs
+### MySQL configs
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -15,13 +15,15 @@ app.config['MYSQL_DATABASE_PASSWORD'] = '#Rocketman84'
 app.config['MYSQL_DATABASE_DB'] = 'blockchain'
 mysql.init_app(app)
 
-# Home
-@app.route('/')
-def Home():
-    return render_template('home.html')
-
+### Home
 @app.route('/', methods=['GET', 'POST'])
 def HomeActionResult():
+    '''
+    Handles actions in home page.
+    RENDER:
+    Home Page
+    '''
+
     if request.method == 'POST':
         # user chose a command
         requestAction = request.form
@@ -33,9 +35,15 @@ def HomeActionResult():
 
     return render_template("home.html")
 
-# Transactions
+### Transactions
 @app.route('/transactions', methods=['POST', 'GET'])
 def Transaction():
+    '''
+    Handles actions in transactions page.
+    RENDER:
+    Transactions Page
+    '''
+
     if request.method == 'POST':
         requestAction = request.form
         # get transaction info from the request
@@ -69,12 +77,14 @@ def Transaction():
         return render_template('transactions.html', transactions=transactions)
     
 
-# Mining
+### Mining
 @app.route('/mine', methods=['GET'])
 def Mine():
     '''
     Performs mining for a block.
+    :return: <dict> New block generated and its data.
     '''
+
     newBlock = appChain.Mine()
     jsonBlock = {
         'index': newBlock.index,
@@ -89,7 +99,9 @@ def Mine():
 
 @app.route('/mining', methods=['GET'])
 def MineLoading():
+    #TODO: Show nonces being generate during mining process to show user mining is being done.
     '''
+    RENDER:
     Loading screen for when mining is happening.
     '''
     return render_template('mining.html')
@@ -97,6 +109,7 @@ def MineLoading():
 @app.route('/mining/completed', methods=['POST', 'GET'])
 def MininingCompleted():
     '''
+    Handles actions in mining completed page.
     Show information for mined block when mining is completed.
     '''
     if request.method == 'POST':
@@ -107,6 +120,7 @@ def MininingCompleted():
             return redirect(url_for('MineLoading'))
     
     else:
+        # Render new block mined.
         newBlock = appChain.LastBlock
         return render_template('miningCompleted.html', newBlock=newBlock)
 
