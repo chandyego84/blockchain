8e27b7df2efd7e1c264e48acfa60d1a520f40e99
chandyego <chandlerjuego101@gmail.com>
2023-01-17 11:12:45 -0800

    refactoring truncating command

--- misc/commands.py
+++ misc/commands.py
@@ -1,38 +1,46 @@
 import mysql.connector
 import sys
 
-### Connect to the database
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="#Rocketman84",
-    database="blockchain"
-)
-cursor = connection.cursor()
+### Necessary arguments to make mysql connection
+password = ""
+database = ""
 
 ### Commands
-
 def TruncatePendingTransactions(tableName):
     ''''
     :param tableName: <string> Name of the table in MySQL db to truncate.
     '''
-    sql_query = f"DELETE FROM {tableName}"
+    ### Connect to the database
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=password,
+        database=database
+    )
+    cursor = connection.cursor()
+
+    ### Execute truncating
+    sql_query = f"TRUNCATE {tableName}"
     cursor.execute(sql_query)
     connection.commit()
-    
-### Executing command
-if (len(sys.argv) > 1):
-    command = sys.argv[1]
-    if (command == 'truncate'):
-        if (len(sys.argv) > 2):
-            TruncatePendingTransactions(sys.argv[2])
-    
-        else:
-            print("Please provide a table name to truncate as a second argument.")
 
-else:
-    print("Please provide a command as the first argument.")
+    ### close the cursor and connection
+    cursor.close()
+    connection.close()
 
-### close the cursor and connection
-cursor.close()
-connection.close()
+if __name__ == "__main__":
+    ### Executing command
+    if (len(sys.argv) > 1):
+        # calling the script with command
+        command = sys.argv[1]
+        if (command == 'truncate'):
+            if (len(sys.argv) > 3):
+                password = sys.argv[2]
+                database = sys.argv[3]
+                TruncatePendingTransactions(sys.argv[4])
+        
+            else:
+                print("Please make sure to provide the arguments: password, database, table")
+
+    else:
+        print("Please provide a command to execute.")
