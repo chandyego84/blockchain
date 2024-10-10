7c90b1fa40ae0e70ac3763de48e1a23073cba1df
Chandler Juego <chandlerjuego101@gmail.com>
2024-10-10 12:24:34 -0700

    Removing SQL database. everything is in one session for now

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
-from flask import Flask
-from flask import render_template, redirect, url_for, request
-from flask import jsonify
-from flaskext.mysql import MySQL
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from blockchain import Block, Blockchain
 
 app = Flask(__name__)
 appChain = Blockchain()
-
-### MySQL configs
-mysql = MySQL()
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '#Rocketman84'
-app.config['MYSQL_DATABASE_DB'] = 'blockchain'
-mysql.init_app(app)
 
 ### Home
 @app.route('/', methods=['GET', 'POST'])
@@ -23,17 +12,21 @@ def HomeActionResult():
     RENDER:
     Home Page
     '''
-
     if request.method == 'POST':
-        # user chose a command
         requestAction = request.form
         if 'New Transaction' in requestAction:
             return redirect(url_for('Transaction'))
-        
         elif 'Mine' in requestAction:
             return redirect(url_for('MineLoading'))
-
     return render_template("home.html")
+
+@app.route('/blockchain', methods=['GET'])
+def ViewBlockchain():
+    '''
+    Displays the entire blockchain.
+    '''
+    blockchain = appChain.chain
+    return render_template('blockchain.html', blockchain=blockchain)
 
 ### Transactions
 @app.route('/transactions', methods=['POST', 'GET'])
@@ -43,39 +36,19 @@ def Transaction():
     RENDER:
     Transactions Page
     '''
-
     if request.method == 'POST':
         requestAction = request.form
-        # get transaction info from the request
         sender = requestAction['Sender']
         recipient = requestAction['Recipient']
         amount = float(requestAction['Amount'])
 
-        # add the transaction to the blockchain
+        # Add the transaction to the blockchain
         appChain.NewTransaction(sender, recipient, amount)
-
-        # insert into mySQL pending_transactions table
-        # Connect to the database
-        conn = mysql.connect()
-        # Create a cursor
-        curs = conn.cursor()
-        curs.execute('''
-            INSERT INTO pending_transactions (sender, recipient, amount) 
-            VALUES (%s, %s, %s)''', (sender, recipient, amount))
-        
-        # save to db
-        conn.commit()
-        # Close the cursor and connection
-        curs.close()
-        conn.close()
-
         return redirect('/transactions')
-    
     else:
-        # show all pending transactions
+        # Show all pending transactions
         transactions = appChain.currentTransactions
         return render_template('transactions.html', transactions=transactions)
-    
 
 ### Mining
 @app.route('/mine', methods=['GET'])
@@ -84,7 +57,6 @@ def Mine():
     Performs mining for a block.
     :return: <dict> New block generated and its data.
     '''
-
     newBlock = appChain.Mine()
     jsonBlock = {
         'index': newBlock.index,
@@ -94,12 +66,11 @@ def Mine():
         'proof': newBlock.nonce,
         'difficulty': newBlock.difficulty
     }
-
-    return jsonBlock
+    # Return JSON response
+    return jsonify(jsonBlock)
 
 @app.route('/mining', methods=['GET'])
 def MineLoading():
-    #TODO: Show nonces being generate during mining process to show user mining is being done.
     '''
     Undergoes mining process.
     RENDER:
@@ -107,24 +78,22 @@ def MineLoading():
     '''
     return render_template('mining.html')
 
+### Mining completed with block details
 @app.route('/mining/completed', methods=['POST', 'GET'])
 def MininingCompleted():
     '''
     Handles actions in mining completed page.
-    Show information for mined block when mining is completed.
+    Shows information for mined block, including transactions.
     '''
     if request.method == 'POST':
-        # User wants to mine again
         requestAction = request.form
-
         if 'Mine' in requestAction:
             return redirect(url_for('MineLoading'))
-    
     else:
-        # Render new block mined.
+        # Get the latest mined block
         newBlock = appChain.LastBlock
 
-        
+        # Render new block mined with transactions
         return render_template('miningCompleted.html', newBlock=newBlock)
 
 if __name__ == '__main__':
